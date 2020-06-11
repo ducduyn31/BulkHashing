8970857536ed1235e312547748effa159f848c10
Daniel Ng <ducduy.ababat@gmail.com>
2020-06-11 12:20:07 -0500

    shell log + fix loop

--- run.sh
+++ run.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-for v in 0 .. 10;
+for v in {0..10};
 do
+  echo "JPG: $v blocks"
   python bulkhash.py -e jpg -i /home/duy/dataset/coco/train2017 -o result -b $v
 done
 
-for v in 0 .. 10;
+for v in {0..10};
 do
+  echo "mp3: $v blocks"
   python bulkhash.py -e mp3 -i /home/duy/dataset/cv_corpus_v1/cv-valid-train -o result -b $v
 done
 
-for v in 0 .. 10;
+for v in {0..10};
 do
+  echo "txt: $v blocks"
   python bulkhash.py -e txt -i /home/duy/Projects/GoogleScrapingBot/txt -o result -b $v
 done
 
-for v in 0 .. 10;
+for v in {0..10};
 do
+  echo "pdf: $v blocks"
   python bulkhash.py -e pdf -i /home/duy/Projects/GoogleScrapingBot/pdf -o result -b $v
 done
 
-for v in 0 .. 10;
+for v in {0..10};
 do
+  echo "xml: $v blocks"
   python bulkhash.py -e xml -i /home/duy/Projects/GoogleScrapingBot/xml -o result -b $v
 done
